e1eff528210f4b03d999030374804d12e7d5006c
stewart bornstein <thrifty677@gmail.com>
2024-02-03 12:26:25 +1100

    removed extra changes and left only the seeder update

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,20 +1,4 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all
   end
-
-  def new
-    @pet = Pet.new
-  end
-
-  def create
-    @pet = Pet.new(pet_params)
-    @pet.save
-  end
-
-  private
-
-  # def pet_params
-  #   params.require(:pet).permit(:name, :age, :species, :gender, :price)
-  # end
 end
