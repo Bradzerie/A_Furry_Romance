bc08e11901ca59552fa0f403ec2dc1cd9465cbf8
Rashika Keshwan <keshwan.r@gmail.com>
2024-02-03 11:29:40 +1100

    Creating Dashboard updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
+  # before_action :set_user, only: %i[dashboard]
+
   def home
   end
 
   def dashboard
+    # @user = User.find(params[:user_id])
+    # @pet.user_id = @user
+    # alternative code to use:
+    # @pets = @user.pets
   end
 end
