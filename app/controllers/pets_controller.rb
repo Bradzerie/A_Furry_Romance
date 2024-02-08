327eeaba1395eafe72cc9f2089f536b45a7b8341
exec zsh <nicky0115@gmail.com>
2024-02-08 19:06:21 +1100

    pet-create-delete

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,6 @@
 class PetsController < ApplicationController
+  # before_action :authenticate_user!
+  # before_action :authorize_owner!, only: [:destroy]
 
   def index
     @pets = Pet.all
@@ -7,4 +9,27 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pet_new_path(@pet), notcie: "Your pet is ready for listing"
+    else
+      puts @pet.errors.full_messages
+    end
+  end
+
+  def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+    redirect_to pets_path(@pet.user), status: :see_other
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :age, :species, :description, :price)
+  end
+
 end
